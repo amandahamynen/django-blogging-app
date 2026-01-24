26c9b7e8836d5c4fc803c68edcedb0d657b424cf
Amanda Hämynen <amandahamynen@users.noreply.github.com>
2026-01-24 13:12:11 +0200

    Add models for BlogPosts and Users

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class User(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
