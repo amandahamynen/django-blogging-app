058948b3c56eb948bbc502e237381cfa6dbed920
Amanda Hämynen <amandahamynen@users.noreply.github.com>
2026-01-27 16:09:42 +0200

    Update blog_id -> blogpost_id

--- blogs/urls.py
+++ blogs/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = "blogs"
 urlpatterns = [
     path("", views.blogpost_list, name="blogpost_list"),
-    path("<int:blog_id>/", views.blogpost_detail, name="blogpost_detail"),
+    path("<int:blogpost_id>/", views.blogpost_detail, name="blogpost_detail"),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -5,6 +5,6 @@ def blogpost_list(request):
     blogposts = BlogPost.objects.all()
     return render(request, 'blogs/blogpost_list.html', {'blogposts': blogposts})
 
-def blogpost_detail(request, blog_id):
-    blogpost = get_object_or_404(BlogPost, id=blog_id)
+def blogpost_detail(request, blogpost_id):
+    blogpost = get_object_or_404(BlogPost, id=blogpost_id)
     return render(request, 'blogs/blogpost_detail.html', {'blogpost': blogpost})
