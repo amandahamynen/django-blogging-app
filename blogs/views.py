f8f8b61a2459ee40c6cee1dcf55aafa83dd54a68
Amanda Hämynen <amandahamynen@users.noreply.github.com>
2026-01-27 15:20:44 +0200

    Update error handling in case blogpost does not exist

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from .models import BlogPost
 
@@ -6,5 +7,8 @@ def blogpost_list(request):
     return render(request, 'blogs/blogpost_list.html', {'blogposts': blogposts})
 
 def blogpost_detail(request, blog_id):
-    blogpost = BlogPost.objects.get(id=blog_id)
+    try:
+        blogpost = BlogPost.objects.get(id=blog_id)
+    except BlogPost.DoesNotExist:
+        raise Http404("Blog post does not exist")
     return render(request, 'blogs/blogpost_detail.html', {'blogpost': blogpost})
