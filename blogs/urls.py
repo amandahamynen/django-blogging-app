ffd3dbfd698bdc32f5b7b63534288e423dc36878
Amanda Hämynen <amandahamynen@users.noreply.github.com>
2026-01-27 16:00:21 +0200

    Update namespace for blog urls

--- blogs/urls.py
+++ blogs/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = "blogs"
 urlpatterns = [
     path("", views.blogpost_list, name="blogpost_list"),
     path("<int:blog_id>/", views.blogpost_detail, name="blogpost_detail"),
