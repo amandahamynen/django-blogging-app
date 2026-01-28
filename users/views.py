7641e375f020c38476a4f38f10bfac6001af44e6
Amanda Hämynen <amandahamynen@users.noreply.github.com>
2026-01-28 13:45:10 +0200

    Update navbar to show correct links while logged in/out

--- users/views.py
+++ users/views.py
@@ -28,5 +28,5 @@ def login_view(request):
 def logout_view(request):
     if request.method == 'POST':
         logout(request)
-        return redirect('blogs:blogpost_list')
+        return render(request, 'users/logout.html')
     return redirect('blogs:blogpost_list')
