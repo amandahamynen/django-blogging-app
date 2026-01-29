057defcc54d0609693546d99cf11e7f8a682343e
Amanda Hämynen <amandahamynen@users.noreply.github.com>
2026-01-29 16:14:56 +0200

    Add blogpost editing

--- blogs/models.py
+++ blogs/models.py
@@ -6,6 +6,7 @@ class BlogPost(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=None)
+    private = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
--- blogs/views.py
+++ blogs/views.py
@@ -4,13 +4,31 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='/users/login/')
 def blogpost_list_all(request):
-    blogposts = BlogPost.objects.all()
+    blogposts = BlogPost.objects.filter(private=False)
+    return render(request, 'blogs/blogpost_list.html', {'blogposts': blogposts})
+
+@login_required(login_url='/users/login/')
+def blogpost_list_my(request):
+    blogposts = BlogPost.objects.filter(creator=request.user)
     return render(request, 'blogs/blogpost_list.html', {'blogposts': blogposts})
 
 @login_required(login_url='/users/login/')
 def blogpost_detail(request, blogpost_id):
     blogpost = get_object_or_404(BlogPost, id=blogpost_id)
+    if request.method == 'POST':
+        return redirect('blogs:blogpost_edit', blogpost_id=blogpost.id)
     return render(request, 'blogs/blogpost_detail.html', {'blogpost': blogpost})
+
+@login_required(login_url='/users/login/')
+def blogpost_edit(request, blogpost_id):
+    blogpost = get_object_or_404(BlogPost, id=blogpost_id)
+    if request.method == 'POST':
+        blogpost.title = request.POST.get('title')
+        blogpost.content = request.POST.get('content')
+        blogpost.private = request.POST.get('private') == 'on'
+        blogpost.save()
+        return redirect('blogs:blogpost_detail', blogpost_id=blogpost.id)
+    return render(request, 'blogs/blogpost_edit.html', {'blogpost': blogpost})
 
 @login_required(login_url='/users/login/')
 def blogpost_create(request):
