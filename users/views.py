d00c6c42f8f87905579bb23f1bfd298d3ab77bfb
Amanda Hämynen <amandahamynen@users.noreply.github.com>
2026-01-30 11:40:20 +0200

    Update logout redirect

--- users/views.py
+++ users/views.py
@@ -33,5 +33,5 @@ def login_view(request):
 def logout_view(request):
     if request.method == 'POST':
         logout(request)
-        return render(request, 'users/logout.html')
-    return redirect('blogs:blogpost_list_all')
+        return redirect('users:login')
+    return redirect('users:login')
